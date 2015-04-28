c0af81c240189f7e4a82a7be9376de4f5b520183
mengqi <5b5f7426@gmail.com>
2015-04-28 11:09:53 +0800

    improve transformContent()

--- atom1.0.go
+++ atom1.0.go
@@ -12,7 +12,7 @@ import "fmt"
 import "time"
 import "html"
 import "encoding/xml"
-import myhtml "github.com/m3ng9i/go-utils/html"
+
 
 // Person Constructs: author, contributor element
 type Atom10Person struct {
@@ -134,12 +134,8 @@ func (t *Atom10Text) Html() string {
 
         return inner.Content
 
-    } else if t.Type == "html" {
-        return transformContent(t.Content)
-
     } else {
-        return myhtml.Text2Html(t.Content)
-
+        return transformContent(t.Content)
     }
 }
 
--- utils.go
+++ utils.go
@@ -2,12 +2,19 @@ package feedreader
 
 import "html"
 import "strings"
+import myhtml "github.com/m3ng9i/go-utils/html"
 
 
+// convert content to html
 func transformContent(content string) string {
     if strings.ContainsAny(content, "<>") {
         return content
     } else {
-        return html.UnescapeString(content)
+        c := html.UnescapeString(content)
+        if strings.ContainsAny(c, "<>") {
+            return c
+        } else {
+            return myhtml.Text2Html(c)
+        }
     }
 }
